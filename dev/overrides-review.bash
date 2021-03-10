eda7e8f09126f2096ebcdde305fcac281f8e964f
Alejandro R. Mosteo <amosteo@unizar.es>
2021-03-10 14:48:38 +0100

    Actions thin binding completed

---
+++ dev/overrides-review.bash
@@ -0,0 +1,2 @@
+# To be run after gen/ has been generated (i.e., after colcon build)
+meld src/rclada/gpr_rcl/gen/ src/rclada/gpr_rcl/src/overrides/
--- dev/uberclean.sh
+++ dev/uberclean.sh
@@ -1,6 +1,11 @@
 echo REMEMBER TO RUN THIS SCRIPT EVERY TIME UNTIL ALL THE GENERATION ERRORS IN
 echo ROSIDL_GENERATOR_ADA / RCLADA ARE SOLVED!! IT\'S NOT ENOUGH TO REMOVE
 echo THE build, install FOLDERS!
+echo
+echo Also, note that there are some manual header overrides "(in rclada notably)"
+echo that should be tested for possible elimination on compiler/ros updates.
+echo Barring that, it would be convenient to refresh them from the original
+echo generated file or check them for differences that could be silently breaking.
 
 ./dev/clean.sh
 ./dev/del_autobindings.sh
